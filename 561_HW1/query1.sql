98b7c4f99cc95ce9ad588b4c331757ecd2f47d68
cbooth <58579427+everlastingflame@users.noreply.github.com>
2023-04-10 17:08:49 -0400

    date better

--- 561_HW1/query1.sql
+++ 561_HW1/query1.sql
@@ -5,17 +5,18 @@ GROUP BY cust
 ),
 
 max_sales AS(
-SELECT maximum.cust, max_q, sls.prod, sls.month, sls.day, sls.year, sls.state
+SELECT maximum.cust, max_q, sls.prod, sls.date, sls.state
 FROM grouped_sales maximum, sales sls
 WHERE maximum.cust = sls.cust and max_q = sls.quant
 ),
 
 min_sales AS(
-SELECT minimum.cust, min_Q, sls2.prod, sls2.month, sls2.day, sls2.year, sls2.state
+SELECT minimum.cust, min_Q, sls2.prod, sls2.date, sls2.state
 FROM grouped_sales minimum, sales sls2
 WHERE minimum.cust = sls2.cust and min_q = sls2.quant
 )
 
-SELECT gr.cust, gr.min_q, MIN.prod, MIN.month, MIN.day, MIN.year, MIN.state, gr.max_q, MAX.prod, MAX.month, MAX.day, MAX.year, MAX.state, gr.avg_q
+SELECT gr.cust, gr.min_q, MIN.prod, MIN.date, MIN.state, gr.max_q, MAX.prod, MAX.date, MAX.state, gr.avg_q
 FROM min_sales AS MIN, max_sales as MAX, grouped_sales AS gr
 WHERE MIN.cust = MAX.cust and MIN.cust = gr.cust and MAX.cust = gr.cust
+order by cust
