a8ac6b9b7a0cb85ad94025af33f3690a35da5c8e
cbooth <58579427+everlastingflame@users.noreply.github.com>
2023-05-11 17:22:41 -0400

    Add files via upload

--- 561_HW2/query1.sql
+++ 561_HW2/query1.sql
@@ -29,21 +29,23 @@ lsthnFinal as(
 select lsthn.prod, lsthn.month, lsthn.quant
 from lsthn 
 INNER JOIN avgSLS
-ON lsthn.prod = avgSLS.prod and lsthn.month = avgSLS.month - 1 and lsthn.quant < avgSLS.avgQ
+ON lsthn.prod = avgSLS.prod and lsthn.month = avgSLS.month - 1 and lsthn.quant > avgSLS.avgQ
 ),
 
-t_union as(
+tUnion as(
 select * 
 from grtrFinal UNION 
 	select * 
 	from lsthnFinal
 ),
-t_final as(
-select distinct sales.prod , sales.month, t_union.quant 
+
+tFinal as(
+select distinct sales.prod , sales.month, tUnion.quant 
 from sales
-LEFT JOIN t_union
-ON sales.prod = t_union.prod and sales.month = t_union.month)
-	select prod product, month, count(quant) SALES_COUNT_BETWEEN_AVGS
-	from t_final
-	group by prod, month
-	order by product, month;
+LEFT JOIN tUnion
+ON sales.prod = tUnion.prod and sales.month = tUnion.month)
+
+select prod product, month, count(quant) SALES_COUNT_BETWEEN_AVGS
+from tFinal
+group by prod, month
+order by product, month
